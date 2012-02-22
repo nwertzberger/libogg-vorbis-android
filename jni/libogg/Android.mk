bde8c79453951561df70e50334a13f46c915e794
Nick Wertzberger <wertnick@gmail.com>
2012-02-21 22:50:13 -0600

    Added compielr flags for hardware floating point.

--- jni/libogg/Android.mk
+++ jni/libogg/Android.mk
@@ -4,6 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libogg
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/../include -ffast-math -fsigned-char
+LOCAL_CFLAGS += -march=armv6 -marm -mfloat-abi=softfp -mfpu=vfp
+
 
 LOCAL_SRC_FILES := \
 	bitwise.c \
