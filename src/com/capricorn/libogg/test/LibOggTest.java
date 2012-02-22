54b9cba1ba962c0209be1950117da8aab14cb91c
Nick Wertzberger <wertnick@gmail.com>
2012-02-21 18:36:53 -0600

    Made sure that an ogg vorbis file is generated on the sdcard on application startup.

--- src/com/capricorn/libogg/test/LibOggTest.java
+++ src/com/capricorn/libogg/test/LibOggTest.java
@@ -3,6 +3,10 @@ package com.capricorn.libogg.test;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import org.ideaheap.io.VorbisFileOutputStream;
+
 public class LibOggTest extends Activity
 {
     /** Called when the activity is first created. */
@@ -14,23 +18,28 @@ public class LibOggTest extends Activity
         short buf[] = new short[1000];
         String fname = "/sdcard/testFile.ogg";
 
-        VorbisFileOutputStream s = new VorbisFileOutputStream(fname);
-        // create a square wave
-        for (int j=0; j < 1000; ) {
-            for (int i=0; i < 100 && j < 1000; i++) {
-                buf[j++] = (short) (5000);
+        try {
+            VorbisFileOutputStream s = new VorbisFileOutputStream(fname);
+            // create a square wave
+            for (int j=0; j < 1000; ) {
+                for (int i=0; i < 100 && j < 1000; i++) {
+                    buf[j++] = (short) (5000);
+                }
+                for (int i=0; i < 100 && j< 1000; i++) {
+                    buf[j++] = (short) (-5000);
+                }
             }
-            for (int i=0; i < 100 && j< 1000; i++) {
-                buf[j++] = (short) (-5000);
-            }
-        }
 
-        s.write(buf);
-        for(int i=0; i < 1000; i++) {
             s.write(buf);
-        }
+            for(int i=0; i < 1000; i++) {
+                s.write(buf);
+            }
 
-        // Save our 11 second ogg square wave.
-        s.close();
+            // Save our 11 second ogg square wave.
+            s.close();
+        }
+        catch (IOException e) {
+            // well i guess that didn't go well...
+        }
     }
 }
