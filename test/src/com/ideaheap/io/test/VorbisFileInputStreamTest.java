3d2be9fc932d44808dd38d1be9b79624a11625d1
Nicholas Wertzberger <wertnick@gmail.com>
2012-11-26 18:38:10 -0600

    Added file leak tests.

--- test/src/com/ideaheap/io/test/VorbisFileInputStreamTest.java
+++ test/src/com/ideaheap/io/test/VorbisFileInputStreamTest.java
@@ -6,8 +6,13 @@ import com.ideaheap.io.VorbisInfo;
 import android.test.AndroidTestCase;
 
 public class VorbisFileInputStreamTest extends AndroidTestCase {
+	
+	
+	private static final String EMPTY = "/sdcard/empty.ogg";
+	private static final String SQUARE_WAVE = "/sdcard/squareWave.ogg";
+
 	public void testOpenClose() throws Exception {
-		VorbisFileInputStream s = new VorbisFileInputStream("/sdcard/empty.ogg");
+		VorbisFileInputStream s = new VorbisFileInputStream(EMPTY);
 		s.close();
 		VorbisInfo info = s.getInfo();
 		assertEquals(info.channels , 1);
@@ -18,15 +23,15 @@ public class VorbisFileInputStreamTest extends AndroidTestCase {
 	public void testReadEmpty() throws Exception {
 		short pcmData[] = new short[2048];
 		int samplesRead;
-		VorbisFileInputStream s = new VorbisFileInputStream("/sdcard/empty.ogg");
+		VorbisFileInputStream s = new VorbisFileInputStream(EMPTY);
 		samplesRead = s.read(pcmData);
 		assertEquals(samplesRead, -1);
 		s.close();
 	}
 	public void testEntireReadSquare() throws Exception {
+		VorbisFileInputStream s = new VorbisFileInputStream(SQUARE_WAVE);
 		short pcmData[] = new short[2048];
 		int samplesRead = 0;
-		VorbisFileInputStream s = new VorbisFileInputStream("/sdcard/squareWave.ogg");
 		samplesRead = s.read(pcmData);
 		assertNotSame(samplesRead, -1);
 		while (samplesRead != -1) {
@@ -34,5 +39,12 @@ public class VorbisFileInputStreamTest extends AndroidTestCase {
 			assertNotSame(samplesRead, 0);
 		}
 		s.close();
+	}
+	
+	public void testOpenAndCloseForFileLeak() throws Exception {
+		for (int i=0; i < 50; i++) {
+			VorbisFileInputStream s = new VorbisFileInputStream(SQUARE_WAVE);
+			s.close();
+		}
 	}
 }
--- test/src/com/ideaheap/io/test/VorbisFileOutputStreamTest.java
+++ test/src/com/ideaheap/io/test/VorbisFileOutputStreamTest.java
@@ -1,11 +1,14 @@
 package com.ideaheap.io.test;
 
 import com.ideaheap.io.VorbisFileOutputStream;
+import com.ideaheap.io.VorbisInfo;
 
 import android.test.AndroidTestCase;
 
 public class VorbisFileOutputStreamTest extends AndroidTestCase {
 	
+	private String fname = "/sdcard/whiteNoise.ogg";
+
 	public void testOpenClose() throws Exception {
 		VorbisFileOutputStream s = new VorbisFileOutputStream("/sdcard/empty.ogg");
 		s.close();
@@ -36,7 +39,6 @@ public class VorbisFileOutputStreamTest extends AndroidTestCase {
 	}
 	public void testWhiteNoise() throws Exception {
 		short buf[] = new short[1000];
-		String fname = "/sdcard/whiteNoise.ogg";
 		
 		VorbisFileOutputStream s = new VorbisFileOutputStream(fname);
 		// Write a square wave
@@ -53,4 +55,11 @@ public class VorbisFileOutputStreamTest extends AndroidTestCase {
 		
 	}
 	
+	public void testOpenAndCloseForFileLeak() throws Exception {
+		VorbisInfo vi = new VorbisInfo();
+		for (int i=0; i < 50; i++) {
+			VorbisFileOutputStream s = new VorbisFileOutputStream(fname,vi);
+			s.close();
+		}
+	}
 }
